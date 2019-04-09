3a4a3bced517769fb81a15bb1349d8ea3aa8036f
Somtirtha Roy <rsomtirtha@gmail.com>
2019-04-09 16:55:39 -0700

    refactored code to take model and condition, generating multiple datasets by changing values of different parameters yet to be coded, formatted the output fiels tobe imported into EDD, need to test this, new function to write data

--- generate_test_omics_data.py
+++ generate_test_omics_data.py
@@ -1,7 +1,10 @@
 
 import cobra
 import pandas as pd
+import random
+import os
 
+data_file_path = 'data'
 
 def generate_fake_data(model, condition):
     """
@@ -15,22 +18,25 @@ def generate_fake_data(model, condition):
     # reaction_id of choice passed to the function# hardcoded here for this particular file (Need to convert this to an interactive cli program)
     reaction_id = 'BIOMASS_Ec_iJO1366_core_53p95M'
     # solution = None
-    solution = get_optimized_solution(model, reaction_id)
 
-    get_protemics_transcriptomics_data(model, solution)
+    while condition:
+        print(condition)
+        condition-=1
+        solution = get_optimized_solution(model, reaction_id)
 
-    get_metabolomics_data(model, solution, condition)
+        # get_proteomics_transcriptomics_data(model, solution, condition)
+
+        get_metabolomics_data(model, condition)
 
 
 
-
-def get_metabolomics_data(model, solution, condition):
+def get_metabolomics_data(model, condition):
     metabolomics = {}
     # get metabolites
     for met in model.metabolites:
         # get associated reactions
         for reaction in list(met.reactions):
-            # get dictionary of associated metaboolites and their concentrations
+            # get dictionary of associated metabolites and their concentrations
             for metabolite, conc in reaction._metabolites.items():
                 if metabolite.id == met.id:
                     if met.id not in metabolomics.keys():
@@ -43,12 +49,12 @@ def get_metabolomics_data(model, solution, condition):
 
     metabolomics_dataframe = pd.DataFrame.from_dict(metabolomics, orient='index', columns=['metabolomics_value'])
     # Write the dataframe into a csv file
-    file_name = 'metabolomics_fakedata.csv'
-    metabolomics_dataframe.to_csv(file_name, sep=',', encoding='utf-8')
+    file_name = f'{data_file_path}/metabolomics_fakedata_condition_{condition}.csv'
+    write_data_files(metabolomics_dataframe, "metabolomics")
 
 
 
-def get_protemics_transcriptomics_data(model, solution):
+def get_proteomics_transcriptomics_data(model, solution, condition):
 
     # pre-determined linear constant (NOTE: Allow user to set this via parameter)
     # DISCUSS!!
@@ -78,18 +84,84 @@ def get_protemics_transcriptomics_data(model, solution):
             transcriptomics[gene.id] = proteomics[protein_id]/q
 
 
-    fake_file_name = 'proteomics_fakedata.csv'
+    file_name = f'{data_file_path}/proteomics_fakedata_condition_{condition}.csv'
     proteomics_dataframe = pd.DataFrame.from_dict(proteomics, orient='index', columns=['proteomics_value'])
-    # Write the dataframe into a csv file
-    proteomics_dataframe.to_csv(fake_file_name, sep=',', encoding='utf-8')
+    write_data_files(proteomics_dataframe, "proteomics")
 
-    fake_file_name = 'transcriptomics_fakedata.csv'
+    file_name = f'{data_file_path}/transcriptomics_fakedata_condition_{condition}.csv'
     transcriptomics_dataframe = pd.DataFrame.from_dict(transcriptomics, orient='index', columns=['transcriptomics_value'])
+    write_data_files(transcriptomics_dataframe, "transcriptomics")
+
+
+def write_data_files(dataframe, data_type=None, condition=1):
+    # print(dataframe)
+    # print("\n")
+
+    # create the filenames
+    sample_file_name = f'{data_file_path}/sample_{condition}.csv'
+    omics_file_name = f'{data_file_path}/{data_type}_fakedata_sample_{condition}.csv'
+
     # Write the dataframe into a csv file
-    transcriptomics_dataframe.to_csv(fake_file_name, sep=',', encoding='utf-8')
+    # dataframe.to_csv(file_name, sep=',', encoding='utf-8')
+
+    # create file number one: sample file
+    if not os.path.isfile(sample_file_name):
+        try:
+            with open(sample_file_name, 'w') as fh:
+                fh.write("Line Name,\n")
+                fh.write(f"Sample {condition}")
+        except Exception as ex:
+            print("Error in writing file!")
+            print(ex)
+
+    # create file number two: omics file
+    unit_dict = { "proteomics": 'g/L',\
+                  "transcriptomics": "g/L",\
+                  "metabolomics": "g/L"
+    }
+
+    try:
+        with open(omics_file_name, 'w') as fh:
+            fh.write("Line Name, Measurement Type, Value, Units\n")
+            sample_name = f"Sample {condition}\n"
+            spaces = ' '*len(sample_name)
+            fh.write(sample_name)
+            for index, series in dataframe.iteritems():
+                for id, value in series.iteritems():
+                    fh.write((f"{spaces}{id}, {value}, {unit_dict[data_type]}\n"))
+
+    except Exception as ex:
+        print("Error in writing file!")
+        print(ex)
+
+
+    # for row in dataframe.iteritems():
+    #     # print(row)
+    #     # print("\n")
+    #     # print(row[0])
+    #     # print("===============")
+    #     # print(row[1])
+    #     # print("\n")
+    #     # print(type(row[0]))
+    #     # print("===============")
+    #     # print(type(row[1]))
+    #     # print("\n")
+    #     for id, value in row[1].iteritems():
+    #         pass
+    #
+    #     import sys
+    #     sys.exit()
 
 
 
+
+
+def get_random_number():
+    random.seed(12312)
+    return random.random()
+
+def add_random_noise():
+    pass
 
 
 def get_list_of_reactions(file_name):
@@ -153,7 +225,7 @@ def read_model(file_name):
 
 if __name__ == "__main__":
     # file_name = 'iJR904.json'
-    file_name = 'data/iECIAI39_1322.xml'
+    file_name = f"{data_file_path}/iECIAI39_1322.xml"
     # reaction_id = 'EX_glc__D_e'
 
     # spits out the list of reaction names related to BIOMASS production
